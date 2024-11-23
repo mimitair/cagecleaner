d61ebfad3d848e219962fe694fa7df104d7e35a2
mimitair <miguel.biltjes@student.kuleuven.be>
2024-11-23 15:46:21 +0100

    Ran cagecleaner on the hpc. Made slurm script. Success for 99 genomes. Some issues presented itself: NCBI only collected 94 genomes. Code broke after editing on the vsc due to TabErrors. Have to fix this locally now.

--- programs/cagecleaner/helper.sh
+++ programs/cagecleaner/helper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo Downloading $(cat assemblies.txt | wc -w) genomes...
+#TODO If output folders exist, they need to be deleted:
+
+
+echo Got $(cat assemblies.txt | wc -w) genomes...
 
 # Download the dehydrated genome package:
 datasets download genome accession $(cat assemblies.txt) --dehydrated
-
-# Delete the assemblies file:
-rm assemblies.txt
 
 # Unzip the file in a folder called "genomes". This folder is located two steps up in the data folder
 unzip -d ../../data/genomes ncbi_dataset.zip
@@ -21,13 +21,15 @@ datasets rehydrate --directory ../../data/genomes
 mkdir ../../data/genomes/all
 mv ../../data/genomes/ncbi_dataset/data/GC*/* ../../data/genomes/all
 
-echo Dereplicating genomes...
 
 # Run skDER on the downloaded genomes:
 #TODO play with threads and ani cutoffs
 skder -g ../../data/genomes/all/* -o ../../data/skder_out
 
 echo Dereplication done! $(cat assemblies.txt | wc -w) genomes were converted into $(ls ../../data/skder_out/Dereplicated_Representative_Genomes | wc -w) genomes. 
+
+# Delete the assemblies.txt file
+rm assemblies.txt
 
 # Now we have to write the accession numbers of the dereplicated genomes to a file:
 ls ../../data/skder_out/Dereplicated_Representative_Genomes > dereplicated_assemblies.txt
--- programs/cagecleaner/main.py
+++ programs/cagecleaner/main.py
@@ -14,112 +14,121 @@ import sys
 import subprocess
 
 def get_scaffolds(path_to_binary: str) -> list:
-	"""
-	This function extracts the scaffold IDs from the cblaster binary output file.
-	
-	:param str path_to_binary: Path to the cblaster binary output file.
-	:rtype list: A list containing the scaffold IDs.
-	"""
-	print("Extracting scaffold accession IDs from the input file...")
+        """
+        This function extracts the scaffold IDs from the cblaster binary output file.
+        
+        :param str path_to_binary: Path to the cblaster binary output file.
+        :rtype list: A list containing the scaffold IDs.
+        """
+        print("--- STEP 1 --- \n Extracting scaffold accession IDs from the input file...")
 
-	# Read the file as comma separated, extract the first column (containing the scaffold IDs) as a polars series and convert to a python list
-	scaffolds = pl.read_csv(path_to_binary, truncate_ragged_lines=True).to_series(1).to_list()
+        # Read the file as comma separated, extract the first column (containing the scaffold IDs) as a polars series and convert to a python list
+        scaffolds = pl.read_csv(path_to_binary, truncate_ragged_lines=True).to_series(1).to_list()
 
-	print(f"Extracted {len(scaffolds)} scaffold IDs")
+        print(f"Extracted {len(scaffolds)} scaffold IDs")
 
-	return scaffolds
-	
+        return scaffolds
+        
 
 def get_assemblies(scaffolds: list) -> dict:
-	"""
-	This function obtains the genome assembly ID for each scaffold ID in the cblaster binary output file.
-	The assembly IDs are then written to a file 'assemblies.txt' as a long string to be passed as an argument to a helper bash script
+        """
+        This function obtains the genome assembly ID for each scaffold ID in the cblaster binary output file.
+        The assembly IDs are then written to a file 'assemblies.txt' as a long string to be passed as an argument to a helper bash script
 
-	:param list scaffolds: A list containing scaffold IDs.
-	:rtype dict: A dictionary with scaffold IDs as keys and assembly IDs as values.
-	"""
-	print("Contacting the NCBI servers and retreiving genome assembly IDs for each hit. This may take a while...")
-	
-	# Create empty dictionary to store genome assembly IDs with scaffold IDs as keys:
-	result = {}
-	
-	for scaffold in scaffolds:
-		
-		# TODO First we have to check if the scaffold accession ID exists in the NCBI database
-		# TODO use esummary or esearch? some scaffold IDs are recognized by esummary but not by esearch
-		# TODO check for metagenome assembled genomes?
-		
-		# For each scaffold ID, we extract the corresponding genome assembly through the NCBI BioProject database:
-		# We use the NCBI E-direct utilities, available from the command line.
-		# TODO check if a genome assembly even exists
-		# TODO check if we can use bioproject id and get th genomes from there. We might not need to go all the way to accession level in this step
-		COMMAND = f"esummary -db nucleotide -id {scaffold} | xtract -pattern DocumentSummary -element BioSample | elink -db biosample -target assembly | efetch -format docsum | xtract -pattern DocumentSummary -element AssemblyAccession"
-		process = subprocess.run(COMMAND, shell=True, capture_output=True, text=True, check=True)	
-	
-		#TODO CHECK FOR DUPLICATES?	
-		# Append the output of the above command to the dictionary:
-		result[scaffold] = process.stdout.strip()
-		#print(process.stdout.strip())	
-		
-	# Print how many assembly IDs were extracted. This should equal the amount of scaffold IDs
-	print(f"Extracted {len(result.values())} assembly IDs ")	
-	print(result)
+        :param list scaffolds: A list containing scaffold IDs.
+        :rtype dict: A dictionary with scaffold IDs as keys and assembly IDs as values.
+        """
+        print("--- STEP 2 --- \n Contacting the NCBI servers and retreiving genome assembly IDs for each hit. This may take a while...")
+        
+        # Create empty dictionary to store genome assembly IDs with scaffold IDs as keys:
+        result = {}
+        
+        for scaffold in scaffolds:
+                
+                # TODO First we have to check if the scaffold accession ID exists in the NCBI database
+                # TODO use esummary or esearch? some scaffold IDs are recognized by esummary but not by esearch
+                # TODO check for metagenome assembled genomes?
+                
+                # For each scaffold ID, we extract the corresponding genome assembly through the NCBI BioProject database:
+                # We use the NCBI E-direct utilities, available from the command line.
+                # TODO check if a genome assembly even exists
+                # TODO check if we can use bioproject id and get th genomes from there. We might not need to go all the way to accession level in this step
+                process = subprocess.run(f"esummary -db nucleotide -id {scaffold} | xtract -pattern DocumentSummary -element BioSample | elink -db biosample -target assembly | efetch -format docsum | xtract -pattern DocumentSummary -element AssemblyAccession", shell=True, capture_output=True, text=True, check=True)    
+        
+                #TODO CHECK FOR DUPLICATES?     
+                # Append the output of the above command to the dictionary:
+                if process.stdout().strip() in result.values():  # This if-statement checks if the genome assembly ID is a duplicate
+                    continue
+                else:
+                    result[scaffold] = process.stdout().strip()
+                
+        # Print how many assembly IDs were extracted. This should equal the amount of scaffold IDs
+        print(f"Obtained {len(result.values())} assembly IDs ") 
+        #print(result)
 
-	return result
-	
+        return result
+        
 
-def cluster_genomes(assemblies: list) -> None:
-	"""
-	This function takes a list of genome assembly IDs and calls a helper bash script that downloads and dereplicates the genomes
+def dereplicate_genomes(assemblies: list) -> None:
+        """
+        This function takes a list of genome assembly IDs and calls a helper bash script that downloads and dereplicates the genomes
+        A file called "dereplicated_assemblies.txt" is generated.
 
-	:param list assemblies: A list of assembly IDs
-	"""
-	# First we write the assembly IDs to a file that can be used by the helper bash script:	
-	with open('assemblies.txt', 'w') as file:
-		file.write(' '.join(str(assembly) for assembly in assemblies))
+        :param list assemblies: A list of assembly IDs
+        """
 
-	# Run the bash script to download and cluster genomes:
-	subprocess.run("./helper.sh", shell=True, check=True)
+        print("--- STEP 3 --- \n Downloading and dereplicating genomes.")
+        # First we write the assembly IDs to a file that can be used by the helper bash script: 
+        with open('assemblies.txt', 'w') as file:
+                file.write(' '.join(str(assembly) for assembly in assemblies))
 
-	# The helper script writes the assembly IDs of the dereplicated genomes to a file called "dereplicated_assemblies.txt".
-	
-	#TODO CHECK OUTPUT VALIDITY CATCH ERRORS
-	
+        # Run the bash script to download and cluster genomes:
+        subprocess.run("./helper.sh", shell=True, check=True)
+        
+        #TODO CHECK OUTPUT VALIDITY CATCH ERRORS
+        
+def extract_cleaned_hits(dereplicated_assemblies: str, scaff_ass_pairs:dict) -> None:
+        
+        # The dereplicated genome assembly IDs are now stored in the file "dereplicated_assemblies.txt" on separate lines
+        # We read this file and couple the assembly IDs back to their scaffold IDs through the dictionary.
+
+        print("--- STEP 4 --- \n Generating cleaned binary output file...")
+
+        with open(dereplicated_assemblies, 'r') as file:
+                for line in file:
+                        # Get the assembly accession out of the file name:
+                        # file names for the genomes are structured as follows: [GCF][_][nine digits][.][version][_][ASMblabla][.fna]
+                        # We only need the part before the second "_" character. Hence, we split the string two times based on the "_" character
+                        # and then join the first two parts back toegther. This results in the assembly ID as it is found in the dictionary
+                        assembly = "_".join(line.split("_", 2)[:2])
+                        
+                        # Now we extract the corresponding scaffold ID. The index of the assembly ID in the dictionary is calculated.
+                        # Then, this index is passed onto the list of keys to get the element at that specific index.
+                        scaffold = list(scaff_ass_pairs.keys())[list(scaff_ass_pairs.values()).index(assembly.strip())]
+                        
+                        # Now we have to match this scaffold ID with the corresponding hit in the original cblaster binary file.
+                        subprocess.run(f"grep '{scaffold}' {path_to_binary} >> ../../data/output/cleaned_binary.csv", shell=True, check=True)
+
+
 
 def main():
-	# Path to the cblatser binary output file, given as command line argument	
-	path_to_binary = sys.argv[1]
-	
-	#TODO CHECK VALIDITY OF FILE
-	
-	# First we capture the dictionary with scaffold:assembly pairs:
-	scaff_ass_pairs = get_assemblies(get_scaffolds(path_to_binary))
+        # Path to the cblatser binary output file, given as command line argument       
+        path_to_binary = sys.argv[1]
+        
+        #TODO CHECK VALIDITY OF FILE (is it csv? is it cblaster output?)
+        
+        # First we capture the dictionary with scaffold:assembly pairs:
+        scaff_ass_pairs = get_assemblies(get_scaffolds(path_to_binary))
 
-	# Then we download and cluster the genomes:
-	cluster_genomes(scaff_ass_pairs.values())
+        # Then we download and cluster the genomes:
+        # This generates a file called "dereplicated_assemblies.txt"
+        dereplicate_genomes(scaff_ass_pairs.values())
 
-	# The dereplicated genome assembly IDs are now stored in the file "dereplicated_assemblies.txt" on separate lines
-	# We read this file and couple the assembly IDs back to their scaffold IDs through the dictionary.
-
-	print("Generating cleaned binary output file...")
-
-	with open('dereplicated_assemblies.txt', 'r') as file:
-		for line in file:
-			# Get the assembly accession out of the file name:
-			# file names for the genomes are structured as follows: [GCF][_][nine digits][.][version][_][ASMblabla][.fna]
-			# We only need the part before the second "_" character. Hence, we split the string two times based on the "_" character
-			# and then join the first two parts back toegther. This results in the assembly ID as it is found in the dictionary
-			assembly = "_".join(line.split("_", 2)[:2])
-			
-			# Now we extract the corresponding scaffold ID. The index of the assembly ID in the dictionary is calculated.
-			# Then, this index is passed onto the list of keys to get the element at that specific index.
-			scaffold = list(scaff_ass_pairs.keys())[list(scaff_ass_pairs.values()).index(assembly.strip())]
-			
-			# Now we have to match this scaffold ID with the corresponding hit in the original cblaster binary file.
-			subprocess.run(f"grep '{scaffold}' {path_to_binary} > ../../data/output/cleaned_binary.csv", shell=True, check=True)
-
-	print("All done! Results are written to '/data/output/cleaned_binary.csv'. Thank you for using cagecleaner :)")
+        # Last step: Extract BGC hits from the original input file based on dereplicated genomes:
+        extract_cleaned_hits('dereplicated_assemblies.txt', scaff_ass_pairs)
+        
+        print("All done! Results are written to '/data/output/cleaned_binary.csv'. Thank you for using cagecleaner :)")
 
 if __name__ == "__main__":
-	main()
-	
+        main()
+        
