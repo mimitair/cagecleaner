ce32bf678bfe0b67e869ec1cf3828c558d451d27
mimitair <miguel.biltjes@student.kuleuven.be>
2024-11-24 12:06:29 +0100

    Code cleanup. Added prevention of duplicate assembly IDs. Fixed TabError issue. Ready to run on hpc again.

--- programs/cagecleaner/helper.sh
+++ programs/cagecleaner/helper.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 #TODO If output folders exist, they need to be deleted:
+if [ -d "../../data/genomes" ]; then
+    rm -r ../../data/genomes
+fi
 
+if [ -f "../../data/output/cleaned_binary.csv" ]; then
+    rm ../../data/output/cleaned_binary.csv
+fi
 
 echo Got $(cat assemblies.txt | wc -w) genomes...
 
@@ -11,9 +17,6 @@ datasets download genome accession $(cat assemblies.txt) --dehydrated
 # Unzip the file in a folder called "genomes". This folder is located two steps up in the data folder
 unzip -d ../../data/genomes ncbi_dataset.zip
 
-# Remove the zip file
-rm ncbi_dataset.zip
-
 # Rehydrate:
 datasets rehydrate --directory ../../data/genomes
 
@@ -21,16 +24,14 @@ datasets rehydrate --directory ../../data/genomes
 mkdir ../../data/genomes/all
 mv ../../data/genomes/ncbi_dataset/data/GC*/* ../../data/genomes/all
 
-
 # Run skDER on the downloaded genomes:
 #TODO play with threads and ani cutoffs
 skder -g ../../data/genomes/all/* -o ../../data/skder_out
 
 echo Dereplication done! $(cat assemblies.txt | wc -w) genomes were converted into $(ls ../../data/skder_out/Dereplicated_Representative_Genomes | wc -w) genomes. 
 
-# Delete the assemblies.txt file
-rm assemblies.txt
-
 # Now we have to write the accession numbers of the dereplicated genomes to a file:
 ls ../../data/skder_out/Dereplicated_Representative_Genomes > dereplicated_assemblies.txt
 
+# CLEAN UP:
+rm ncbi_dataset.zip assemblies.txt
